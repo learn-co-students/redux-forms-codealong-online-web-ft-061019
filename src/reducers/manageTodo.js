a2dc0d9019c8aeb24c98297d7a1fefb1d94b0d0b
Mic Fox <michael.c.fox.ii@gmail.com>
2019-11-06 16:03:23 -0500

    Done.

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -1,8 +1,11 @@
 export default function manageTodo(state = {
   todos: [],
 }, action) {
-
+  switch (action.type) {
+    case 'ADD_TODO': 
+  
   console.log("reducer received this action:", action);
 
   return state;
+  }
 }
